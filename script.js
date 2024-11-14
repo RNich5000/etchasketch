0e89fa34b0e6e67e6f099bd6ecb23ff4770f1a78
Rob Nicholls <robertnicholls@gmail.com>
2024-11-14 17:57:59 +1100

    add randomizer

--- script.js
+++ script.js
@@ -1,36 +1,58 @@
 const container = document.querySelector(".container");
 const btnReset = document.querySelector("#reset");
+const btnRandomize = document.querySelector("#randomize");
+const btnDarken = document.querySelector("#darken");
 
 btnReset.addEventListener("click", () => {
-	while (container.firstChild10) {
-		container.removeChild(container.lastChild);
-	}
-	startProgram();
+	startProgram(false);
 });
 
-const addCell = function () {
-	const cell = document.createElement("div");
-	cell.classList.add("cell");
-	cell.classList.add("dark");
-	container.appendChild(cell);
+btnRandomize.addEventListener("click", () => {
+	startProgram(true);
+});
 
-	cell.addEventListener("mouseover", () => {
-		cell.classList.toggle("dark");
-	});
-};
+btnDarken.addEventListener("click", () => {
+	startProgram(false);
+});
 
-const populateGrid = function (num) {
-	for (let i = 0; i < num ** 2; i++) {
-		addCell();
+const clearBoard = function () {
+	while (container.firstChild) {
+		container.removeChild(container.lastChild);
 	}
 };
 
-const createCssRule = function (num) {
+const addCell = function (isRandom) {
+	const cell = document.createElement("div");
+	cell.classList.add("cell");
+	container.appendChild(cell);
+
+	if (isRandom) {
+		cell.addEventListener("mouseover", () => {
+			const red = Math.floor(Math.random() * 256).toString();
+			const green = Math.floor(Math.random() * 256).toString();
+			const blue = Math.floor(Math.random() * 256).toString();
+			const color = `RGB(${red},${green},${blue})`;
+			cell.style.backgroundColor = color;
+		});
+	} else {
+		cell.classList.add("light");
+		cell.addEventListener("mouseover", () => {
+			cell.classList.toggle("light");
+		});
+	}
+};
+
+const populateGrid = function (num, isRandom) {
+	for (let i = 0; i < num ** 2; i++) {
+		addCell(isRandom);
+	}
+};
+
+const createBoxSize = function (num) {
 	const cells = document.querySelectorAll(".cell");
 	const style = window.getComputedStyle(container);
 	const width = parseInt(style.getPropertyValue("width").replace("px", ""));
 	const cellSize = width / num;
-	6;
 
 	for (let i = 0; i < cells.length; i++) {
 		cells[i].style.width = cellSize.toString() + "px";
@@ -38,17 +60,19 @@ const createCssRule = function (num) {
 	}
 };
 
-const startProgram = function () {
+const startProgram = function (isRandom) {
+	clearBoard();
+
 	let numStr = prompt("How many squares? (1-100)");
 	let num = parseInt(numStr) || 0;
 
 	if (num > 0 && num <= 100) {
-		populateGrid(num);
-		createCssRule(num);
+		populateGrid(num, isRandom);
+		createBoxSize(num);
 	} else {
 		alert("Please enter a valid integer between 1 and 100.");
 		startProgram();
 	}
 };
 
-startProgram();
+startProgram(false);
